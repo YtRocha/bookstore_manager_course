5c0f9df47c37bc006e93c49aa5968f5ee75a6b3c
Ytallo Daniel <ytallo4gamer@gmail.com>
2021-10-14 19:05:35 -0300

    Developed project entities

---
+++ src/main/java/com/ytrocha/bookstoremanager/entity/Author.java
@@ -0,0 +1,26 @@
+package com.ytrocha.bookstoremanager.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age ;
+}
